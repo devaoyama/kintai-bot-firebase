16f230eb16d928a42d0b88d334454d94b97c8d96
devkeita <keita.mywork@gmail.com>
2020-08-31 00:40:10 +0900

    make users

---
+++ functions/src/firestore/users.ts
@@ -0,0 +1,38 @@
+import * as admin from "firebase-admin";
+import {User} from "../interfaces";
+
+export default class Users {
+    readonly db = admin.firestore();
+
+    private user: User | undefined;
+
+    async init(user: User) {
+        const userData = await this.getData(user.uid);
+        if (!userData) {
+            await this.setData(user);
+        }
+    }
+
+    async getData(uid: string) {
+        const doc = await this.db.collection('users').doc(uid).get();
+        if (!doc.exists) {
+            return null;
+        }
+        // @ts-ignore
+        this.setUser(doc.data());
+        return this.user;
+    }
+
+    async setData(user: User) {
+        await this.db.collection('users').doc(user.uid).set(user);
+        this.setUser(user);
+    }
+
+    getUser() {
+        return this.user;
+    }
+
+    setUser(user: User) {
+        this.user = user;
+    }
+}
--- functions/src/interfaces.ts
+++ functions/src/interfaces.ts
@@ -4,3 +4,10 @@ import Request from "./request";
 export interface Command {
     execute(request: Request, i18n: I18n): string;
 }
+
+export interface User {
+    uid: string,
+    email: string,
+    slack_uid: string,
+    slack_username: string,
+}
